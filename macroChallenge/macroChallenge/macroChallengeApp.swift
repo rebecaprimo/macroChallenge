be7fa96d925fb0660c34ab97cb47bfb632ca6b07
Rebeca Primo <rebecarodriguesprimo036@gmail.com>
2023-05-22 16:52:29 -0300

    projeto criado!

---
+++ macroChallenge/macroChallenge/macroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  macroChallengeApp.swift
+//  macroChallenge
+//
+//  Created by rebeca primo on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct macroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
